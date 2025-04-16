879e6ddeaeea6deebd1c17406d1bdf5b90ef9701
RTMecha <125487712+RTMecha@users.noreply.github.com>
2025-04-16 18:14:37 +1000

    Made the Crafter block more accurate to the 1.21 Crafter by changing placement direction from horizontal direction to nearest looking direction.

--- src/main/java/org/violetmoon/quark/content/automation/block/CrafterBlock.java
+++ src/main/java/org/violetmoon/quark/content/automation/block/CrafterBlock.java
@@ -136,7 +136,7 @@ public class CrafterBlock extends ZetaBlock implements EntityBlock {
 
 	@Override
 	public BlockState getStateForPlacement(BlockPlaceContext ctx) {
-		return this.defaultBlockState().setValue(FACING, ctx.getHorizontalDirection().getOpposite());
+		return this.defaultBlockState().setValue(FACING, ctx.getNearestLookingDirection().getOpposite());
 	}
 
 	@Override
