633a9e57d1ad22870dcd0b6bdc204ac9025bdc91
Partonetrain <Partonetrain@gmail.com>
2026-03-22 17:20:11 -0500

    idk what im doin man...

--- src/main/java/org/violetmoon/quark/content/management/module/QASCASHandler.java
+++ src/main/java/org/violetmoon/quark/content/management/module/QASCASHandler.java
@@ -1,38 +1,64 @@
 package org.violetmoon.quark.content.management.module;
 
 import curiousarmorstands.CuriousArmorStands;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.decoration.ArmorStand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import org.violetmoon.quark.base.Quark;
 import org.violetmoon.zeta.event.play.entity.player.ZPlayerInteract;
 import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.SlotResult;
 import top.theillusivec4.curios.api.type.capability.ICurio;
+import top.theillusivec4.curios.api.type.capability.ICuriosItemHandler;
 import top.theillusivec4.curios.api.type.inventory.ICurioStacksHandler;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 
 //quick armor swapping/curious armor stands module handler.
 public class QASCASHandler {
     public static final String SLOT = "curio"; //all armorstand curios go into this slot
 
     public static void swapCurios(ZPlayerInteract.EntityInteractSpecific event, Player player, ArmorStand armorStand){
-        var playerCurios = CuriosApi.getCuriosInventory(player);
-        List<SlotResult> playerResults = playerCurios.get().findCurios();
+        /*
+        Since the method returns an Optional by default, developers will need to make sure to use Optional#ifPresent first in order to check that the inventory actually exists:
 
-        for(SlotResult result : playerResults) { //it's size 0 at this point for some reason
-            playerToStand(player, armorStand, result);
+        CuriosApi.getCuriosInventory(livingEntity).ifPresent(curiosInventory -> {
+          // code here - with access to the inventory instance that now definitely exists
+        });
+
+        Once a developer has the ICuriosItemHandler instance, they can use the methods from that instance to interact with the Curios inventory.
+         */
+
+        Optional<ICuriosItemHandler> maybeCuriosInventory = CuriosApi.getCuriosInventory(player);
+        ICuriosItemHandler playerCuriosItemhandler = maybeCuriosInventory.get(); //we can safely assume the player has a curios inventory
+
+        maybeCuriosInventory = CuriosApi.getCuriosInventory(armorStand);
+        ICuriosItemHandler standCuriosItemhandler = maybeCuriosInventory.get(); //same for armor stand
+
+        List<ItemStack> playerCurios = new ArrayList<>();
+        List<ItemStack> standCurios = new ArrayList<>();
+
+        for(ICurioStacksHandler slot : playerCuriosItemhandler.getCurios().values()) {
+            int numberOfSlots = slot.getSlots();
+            for (int i = 0; i < numberOfSlots; i++) {
+                ItemStack stack = slot.getStacks().getStackInSlot(i);
+                playerCurios.add(stack);
+            }
         }
 
+        Quark.LOG.info(playerCurios);
+
+        /*
         var standCurios = CuriosApi.getCuriosInventory(armorStand);
         List<SlotResult> standResults = standCurios.get().findCurios();
 
         for(SlotResult result : standResults) {
             standToPlayer(player, armorStand, result);
         }
+
+         */
     }
 
 
